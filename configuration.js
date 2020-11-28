1382e276b2cc60180bab2eca7100688401a95fd6
Guilherme Rangel <guilherme.santana@sita.aero>
2020-11-28 18:14:54 -0300

    Add mongo configuration

--- configuration.js
+++ configuration.js
@@ -1,5 +1,10 @@
 module.exports = {
-    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
-    GoogleSecret: process.env.GOOGLE_SECRET,
-    GoogleRedirectUrl: process.env.REDIRECT_URL
+    Google: {
+        ClientID: process.env.GOOGLE_CLIENT_ID,
+        Secret: process.env.GOOGLE_SECRET,
+        RedirectUrl: process.env.REDIRECT_URL
+    },
+    Mongo: {
+        URI: proccess.env.MONGO_URI
+    }
 }
